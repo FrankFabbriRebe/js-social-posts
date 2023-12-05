f562f79d2154d392cd9a1e1b2a341aebe563df59
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-12-05 15:39:38 +0100

    add: content in html page

--- main.js
+++ main.js
@@ -55,3 +55,46 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// selezione elementi html
+const container = document.getElementById("container");
+console.log(container);
+
+// creazione post in base al numero oggetti contenuti nell'array posts
+posts.forEach((obj) => {
+   
+    container.innerHTML += 
+
+    `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${obj.author.image}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${obj.author.name}</div>
+                    <div class="post-meta__time">${obj.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${obj.content}</div>
+        <div class="post__image">
+            <img src="${obj.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${obj.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+
+});
+
